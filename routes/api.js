2f7be471863c33276f5df9607ee7ba88cbecebb3
Jordy Bastien Rugumbira <rugumbirajordybastien@gmail.com>
2020-04-17 15:56:37 +0200

    feat: Improve the movie service

--- routes/api.js
+++ routes/api.js
@@ -5,13 +5,18 @@ const path = require('path');
 const MovieService = require('../lib/movies');
 const movieService = new MovieService(path.join(__dirname, '..', 'movies.db'));
 
-router.get('/movies/all', function(req, res) {
+router.get('/movies/all', function (req, res) {
   movieService.getAllMovies((err, movies) => {
-    res.json(movies);
+    // map function used to split genres into an array
+    const newMovies = movies.map((movie) => {
+      movie.genres = movie.genres.split('|');
+      return movie;
+    });
+    res.json(newMovies);
   });
 });
 
-router.get('/movies/byGenre/:genre', function(req, res) {
+router.get('/movies/byGenre/:genre', function (req, res) {
   movieService.getMoviesByGenre(req.params.genre, (err, movies) => {
     res.json(movies);
   });
